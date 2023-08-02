2b69e9a71f0c2933877c23d4291896239370575e
Simon Thing-Yew Yin <simon.y@quantum-brilliance.com>
2023-08-02 00:31:36 +0000

    Try again with GA...

--- docs/_static/js/qb_ga.js
+++ docs/_static/js/qb_ga.js
@@ -1,7 +1,4 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-215J15FW78"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-215J15FW78');
-</script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-215J15FW78');
