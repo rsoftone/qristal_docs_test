3c7e5907296311a980c8aacb82df4194e2f517ef
Simon Thing-Yew Yin <simon.y@quantum-brilliance.com>
2023-08-06 23:59:56 +0000

    Clarity tracker added

--- docs/_static/js/qb_ga.js
+++ docs/_static/js/qb_ga.js
@@ -1,4 +1,9 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-215J15FW78');
+gtag('config', 'G-EML76VL6ZZ');
+(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "i8ah5yqrew");
